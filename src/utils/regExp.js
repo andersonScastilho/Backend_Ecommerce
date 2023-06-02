3a141642bfa768463e151c6a904fd6915dcf3b4e
Anderson Leonardo <131002730+SuportePapel@users.noreply.github.com>
2023-06-02 10:10:07 -0300

    fix: corrigindo regex

--- src/utils/regExp.js
+++ src/utils/regExp.js
@@ -1,3 +1,3 @@
-export const regExpIsValidText = value => /^[a-z]\D/gi.test(value)
+export const regExpIsValidText = value => /^[^\d]+$/gi.test(value)
 
 export const isValidBRZip = zip => /^[0-9]{5}-[0-9]{3}$/.test(zip);
