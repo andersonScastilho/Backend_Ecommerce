60e30f275f3aeb378c6d62700087ec1eb9af01c2
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-06-02 22:58:45 -0300

    adicionando as validações

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -6,14 +6,15 @@ import joiSchemaValidator from '../middlewares/joiSchemaValidator'
 
 import productController from "../controllers/ProductController";
 
-import createProductSchema from '../schemas/products/productSchema'
+import createProductSchema from '../schemas/products/createProductSchema'
+import updateProductSchema from '../schemas/products/updateProductSchema'
 
 const router = new Router();
 
 router.post("/", loginAdmUserRequired, validUserRole, joiSchemaValidator(createProductSchema), productController.store);
 router.get("/", productController.index);
 
-router.put("/:product_id", loginAdmUserRequired, validUserRole, productController.update);
+router.put("/:product_id", loginAdmUserRequired, validUserRole, joiSchemaValidator(updateProductSchema), productController.update);
 router.delete("/:product_id", loginAdmUserRequired, validUserRole, productController.delete);
 router.get("/:product_id", productController.show);
 
